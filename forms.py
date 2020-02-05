c00dc464ff823a6c252758827af7739d512cfc6c
shweper <32496204+shweper@users.noreply.github.com>
2020-02-05 13:47:49 +0300

    Add files via upload

--- forms.py
+++ forms.py
@@ -3,17 +3,24 @@ import pandas as pd
 
 browser = Browser('chrome')
 # browser.driver.set_window_size(640, 480)
-browser.visit('https://yandex.ru')
-search_bar_xpath = '//*[@id="text"]'
-search_bar = browser.find_by_xpath(search_bar_xpath)[0]
-search_bar.fill('splinter - python acceptance testing for web applications')
+browser.visit('https://nn.rabota.ru/v3_myVacancy.html?action=create&company_registered=true&employer_registered=true')
+# Логинимся
+login_bar_xpath = '//*[@id="mail"]'
+login_bar = browser.find_by_xpath(login_bar_xpath)[0]
+login_bar.fill('shweper@ya.ru')
 
-search_button_xpath = '/html/body/div[1]/div[2]/div[3]/div/div[2]/div/fwap/dhtaq/aqwf/div/div[2]/div/div[2]/div/form'
-search_button = browser.find_by_xpath(search_button_xpath)[0]
-search_button.click()
+pass_bar_xpath = '//*[@id="password"]'
+pass_bar = browser.find_by_xpath(pass_bar_xpath)[0]
+pass_bar.fill('Fqtawe98')
+# кликкаем войти
+browser.find_by_xpath('//*[@id="authForm"]/input[5]').click()
+
+a = 100
+vak_bar_xpath = '//*[@id="custom_position"]'
+vak_bar = browser.find_by_xpath(vak_bar_xpath)[0]
+vak_bar.fill(a)
 
 
 
 
-
-browser.quit()
+# browser.quit()
