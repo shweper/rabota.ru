934106aa5ca0f893c1ff05dd51177ef03329c4db
shweper <32496204+shweper@users.noreply.github.com>
2020-02-05 15:01:14 +0300

    Add files via upload

---
+++ forms.py
@@ -0,0 +1,26 @@
+from splinter import Browser
+import pandas as pd
+
+browser = Browser('chrome')
+# browser.driver.set_window_size(640, 480)
+browser.visit('https://nn.rabota.ru/v3_myVacancy.html?action=create&company_registered=true&employer_registered=true')
+# Логинимся
+login_bar_xpath = '//*[@id="mail"]'
+login_bar = browser.find_by_xpath(login_bar_xpath)[0]
+login_bar.fill('переменная логин')
+
+pass_bar_xpath = '//*[@id="password"]'
+pass_bar = browser.find_by_xpath(pass_bar_xpath)[0]
+pass_bar.fill('переменная pass')
+# кликкаем войти
+browser.find_by_xpath('//*[@id="authForm"]/input[5]').click()
+
+a = 100
+vak_bar_xpath = '//*[@id="custom_position"]'
+vak_bar = browser.find_by_xpath(vak_bar_xpath)[0]
+vak_bar.fill(a)
+
+
+
+
+# browser.quit()
