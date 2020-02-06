32c21ca17db1cf6de30142db59e97c3725f92a0a
vitekmoremm <60715565+vitekmoremm@users.noreply.github.com>
2020-02-06 22:07:59 +0300

    Add files via upload

--- script3.py
+++ script3.py
@@ -1,6 +1,7 @@
 from openpyxl import load_workbook
 import random
 
+
 wb = load_workbook('./DATA.xlsx')
 lst = (wb.sheetnames)
 
@@ -9,23 +10,29 @@ lst = (wb.sheetnames)
 # sheet = wb[random.choice(lst)]
 sheet = wb[lst[1]]
 sheet.title
-
+#print(sheet.cell(row=1, column=1).value)
 # random.randint(A, B) - случайное целое число N, A ≤ N ≤ B.
-gor = 2
-adr = 2
-for row in sheet.iter_cols(max_col=1):
-    for value in row:
-        gor = gor+1
 
-for col in sheet.iter_rows(max_row=1):
-    for value in col:
-        adr = adr+1
+a=0
+for cell in sheet['A']:
+    if cell.value == None:
+        break
+    else:
+         a = a + 1
+         #print(cell.value)
+a=a-1
+element = 2
 
-random_gor = random.randint(2, gor-2)
-random_adr = random.randint(2, adr-2)
-print(gor, adr)
-gorod = (sheet.cell(row=random_gor, column=1).value)
-addres = (sheet.cell(row=random_gor, column=random_adr).value)
+for element in range(a):
+    element = element + 1
+    for cell in list(sheet.rows)[element]:
+     print(str(cell.value))
+print(element)
+#random_gor = random.randint(2, gor-2)
+#random_adr = random.randint(2, adr-2)
+#print(gor, adr)
+#gorod = (sheet.cell(row=random_gor, column=1).value)
+#addres = (sheet.cell(row=random_gor, column=random_adr).value)
 
-print(gorod)
-print(addres)
+#print(gorod)
+#print(addres)
