fbfa8fc5b0b8a3abcfcccf92c3b92d7c5f79bf30
vitekmoremm <60715565+vitekmoremm@users.noreply.github.com>
2020-02-08 02:24:02 +0300

    Add files via upload

--- script3.py
+++ script3.py
@@ -5,34 +5,60 @@ import random
 wb = load_workbook('./DATA.xlsx')
 lst = (wb.sheetnames)
 
-# print(random.choice(lst))
-# Берём рандомный лист
-# sheet = wb[random.choice(lst)]
 sheet = wb[lst[1]]
 sheet.title
-#print(sheet.cell(row=1, column=1).value)
 # random.randint(A, B) - случайное целое число N, A ≤ N ≤ B.
 
-a=0
-for cell in sheet['A']:
-    if cell.value == None:
-        break
-    else:
-         a = a + 1
-         #print(cell.value)
-a=a-1
-element = 2
+barnaul_adr = []
+volgograd_adr = []
+voronej_adr = []
+ekaterenburg_adr = []
+izevsk_adr = []
+irkuts_adr = []
+kazan_adr = []
+kaliningrad_adr = []
+kemerovo_adr = []
+krasnodar_adr = []
+krasnoyars_adr = []
+moskow_adr = []
+naber_cheln_adr = []
+nizniy_novgorod_adr = []
+novosib_adr = []
+omsk_adr = []
+orenburg_adr = []
+perm_adr = []
+rostov_na_dony_adr = []
+ryazan_adr = []
+samara_adr = []
+sankt_peter_adr = []
+saratov_adr = []
+sochi_and_adler = []
+toliatty_adr = []
+tomsk_adr = []
+tyla_adr = []
+tumen_adr = []
+ufa_adr = []
+chelabinsk_adr = []
+yaroslavl_adr = []
+musor_exela = []
+goroda_arr = [musor_exela, barnaul_adr, volgograd_adr, voronej_adr, ekaterenburg_adr, izevsk_adr, irkuts_adr, kazan_adr, kaliningrad_adr, kemerovo_adr, krasnodar_adr, krasnoyars_adr, moskow_adr, naber_cheln_adr, nizniy_novgorod_adr, novosib_adr, omsk_adr, orenburg_adr, perm_adr, rostov_na_dony_adr, ryazan_adr, samara_adr, sankt_peter_adr, saratov_adr, sochi_and_adler, toliatty_adr, tomsk_adr, tyla_adr, tumen_adr, ufa_adr, chelabinsk_adr, yaroslavl_adr]
+number_gorod = 0
+#for cell in sheet[goroda_arr[number_gorod]]:
+#    if cell.value == None:
+#        break
+kolichestvo_gorodov = 32
+for number_gorod in range(kolichestvo_gorodov):
+    for cell in list(sheet.rows)[number_gorod]:
+        if cell.value == None:
+            number_gorod = number_gorod +1
+            break
+        else:
+            goroda_arr[number_gorod].append(cell.value)
+        # goroda_arr[number_adr].pop(0)
 
-for element in range(a):
-    element = element + 1
-    for cell in list(sheet.rows)[element]:
-     print(str(cell.value))
-print(element)
-#random_gor = random.randint(2, gor-2)
-#random_adr = random.randint(2, adr-2)
-#print(gor, adr)
-#gorod = (sheet.cell(row=random_gor, column=1).value)
-#addres = (sheet.cell(row=random_gor, column=random_adr).value)
+    #print(str(cell.value))
+#print(element)
+goroda_arr.pop(0)
+print(goroda_arr)
 
-#print(gorod)
-#print(addres)
+print(range(kolichestvo_gorodov))
