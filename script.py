046db77723f99a89d027f0b002ab59769a919d05
shweper <32496204+shweper@users.noreply.github.com>
2020-02-07 18:13:53 +0300

    Add files via upload

--- script.py
+++ script.py
@@ -2,6 +2,13 @@ from openpyxl import load_workbook
 import random
 from splinter import Browser
 # import pandas as pd
+import xlrd
+wb = xlrd.open_workbook('DATA.xlsx')
+sheet_xlr = wb.sheet_by_name('Вакансии')
+a = 2
+rows_sheet = sheet_xlr.nrows
+print(rows_sheet)
+
 
 wb = load_workbook('./DATA.xlsx')
 lst = (wb.sheetnames)
@@ -13,13 +20,13 @@ sheet = wb[lst[0]]
 sheet.title
 
 # random.randint(A, B) - случайное целое число N, A ≤ N ≤ B.
-a = 0
+#a = 0
 # b = [a]
 
-for row in sheet.iter_cols(max_col=1):
-    for value in row:
-        a = a+1
-print(a)
+#for row in sheet.iter_cols(max_col=1):
+#    for value in row:
+#        a = a+1
+#print(a)
 
 # альтернативный рандом
 #        b.append(a)
@@ -29,7 +36,7 @@ print(a)
 # print(b)
 # i = random.choice(b)
 
-i = random.randint(3, a)
+i = random.randint(3, rows_sheet)
 ################### заводим содержимое в переменные ################################
 # print(a)
 vacancy = (sheet.cell(row=i, column=1).value)
@@ -51,7 +58,7 @@ dni_priem = (sheet.cell(row=i, column=16).value)
 company = (sheet.cell(row=i, column=17).value)
 opis_company = (sheet.cell(row=i, column=18).value)
 # gorod = (sheet.cell(row=i, column=18).value)
-print(opisanie)
+# print(priem_zv_c)
 
 ###############     Browser     ###############
 
@@ -88,7 +95,7 @@ browser.find_by_xpath('//*[@id="vacancyForm"]/div[2]/div/div/table/tbody/tr[1]/t
 if opyt == 0:
     browser.find_by_xpath('//*[@id="offer_experience_year_count"]/option[4]').click()
 elif opyt == 1:
-    browser.find_by_xpath('// *[ @ id = "offer_experience_year_count"] / option[5]').click()
+    browser.find_by_xpath('//*[ @ id = "offer_experience_year_count"] / option[5]').click()
 elif opyt == 2:
     browser.find_by_xpath('//*[@id="offer_experience_year_count"]/option[6]').click()
 elif opyt == 3:
@@ -145,11 +152,11 @@ vozr_do_bar = browser.find_by_xpath(vozr_do_bar_xpath)[0]
 vozr_do_bar.fill(vozr_do)
 
 ###############     Описание вакинсии     ###############
-browser.find_by_xpath('//*[@id="mce_3"]/button').click()
+#browser.find_by_xpath('//*[@id="mce_3"]/button').click()
 
-# browser.find_by_xpath('//*[@id="mce_16"]').click()
-# browser.find_by_xpath('//*[@id="description_ifr"]').fill(opisanie)
-
+# browser.find_by_tag('html').click()
+q = browser.find_by_id('tinymce').first
+q.fill('qwereewrqw')
 # opisanie_bar_xpath = '//*[@id="tinymce"]'
 # opisanie_bar = browser.find_by_xpath(opisanie_bar_xpath)[0]
 # opisanie_bar.fill(opisanie)
@@ -177,9 +184,105 @@ phone_bar.fill(phone)
 
 ###############    время для звонков    ###############
 
+if priem_zv_c == '0:00:00':
+    browser.find_by_xpath('//*[@id="phoneContainerCallFrom_"]/option[1]').mouse_over()
+    browser.find_by_xpath('//*[@id="phoneContainerCallFrom_"]/option[1]').click()
+elif priem_zv_c == '0:30:00':
+    browser.find_by_xpath('//*[@id="phoneContainerCallFrom_"]/option[1]').mouse_over()
+    browser.find_by_xpath('//*[@id="phoneContainerCallFrom_"]/option[2]').click()
+elif priem_zv_c == '1:00:00':
+    browser.find_by_xpath('//*[@id="phoneContainerCallFrom_"]/option[3]').click()
+elif priem_zv_c == '1:30:00':
+    browser.find_by_xpath('//*[@id="phoneContainerCallFrom_"]/option[4]').click()
+elif priem_zv_c == '2:00:00':
+    browser.find_by_xpath('//*[@id="phoneContainerCallFrom_"]/option[5]').click()
+elif priem_zv_c == '2:30:00':
+    browser.find_by_xpath('//*[@id="phoneContainerCallFrom_"]/option[6]').click()
+elif priem_zv_c == '3:00:00':
+    browser.find_by_xpath('//*[@id="phoneContainerCallFrom_"]/option[7]').click()
+elif priem_zv_c == '3:30:00':
+    browser.find_by_xpath('//*[@id="phoneContainerCallFrom_"]/option[8]').click()
+elif priem_zv_c == '4:00:00':
+    browser.find_by_xpath('//*[@id="phoneContainerCallFrom_"]/option[9]').click()
+elif priem_zv_c == '4:30:00':
+    browser.find_by_xpath('//*[@id="phoneContainerCallFrom_"]/option[10]').click()
+elif priem_zv_c == '5:00:00':
+    browser.find_by_xpath('//*[@id="phoneContainerCallFrom_"]/option[11]').click()
+elif priem_zv_c == '5:30:00':
+    browser.find_by_xpath('//*[@id="phoneContainerCallFrom_"]/option[12]').click()
+elif priem_zv_c == '6:00:00':
+    browser.find_by_xpath('//*[@id="phoneContainerCallFrom_"]/option[13]').click()
+elif priem_zv_c == '6:30:00':
+    browser.find_by_xpath('//*[@id="phoneContainerCallFrom_"]/option[14]').click()
+elif priem_zv_c == '7:00:00':
+    browser.find_by_xpath('//*[@id="phoneContainerCallFrom_"]/option[15]').click()
+elif priem_zv_c == '7:30:00':
+    browser.find_by_xpath('//*[@id="phoneContainerCallFrom_"]/option[16]').click()
+elif priem_zv_c == '8:00:00':
+    browser.find_by_xpath('//*[@id="phoneContainerCallFrom_"]/option[17]').click()
+elif priem_zv_c == '8:30:00':
+    browser.find_by_xpath('//*[@id="phoneContainerCallFrom_"]/option[18]').click()
+elif priem_zv_c == '9:00:00':
+    browser.find_by_xpath('//*[@id="phoneContainerCallFrom_"]/option[19]').click()
+elif priem_zv_c == '9:30:00':
+    browser.find_by_xpath('//*[@id="phoneContainerCallFrom_"]/option[20]').click()
+elif priem_zv_c == '10:00:00':
+     browser.find_by_xpath('//*[@id="offer_experience_year_count"]/option[12]').click()
+elif priem_zv_c == '10:30:00':
+    browser.find_by_xpath('//*[@id="offer_experience_year_count"]/option[13]').click()
+elif priem_zv_c == '11:00:00':
+    browser.find_by_xpath('//*[@id="offer_experience_year_count"]/option[14]').click()
+elif priem_zv_c == '11:30:00':
+    browser.find_by_xpath('//*[@id="offer_experience_year_count"]/option[2]').click()
+elif priem_zv_c == '12:00:00':
+    browser.find_by_xpath('// *[ @ id = "offer_experience_year_count"] / option[5]').click()
+elif priem_zv_c == '12:30:00':
+    browser.find_by_xpath('//*[@id="offer_experience_year_count"]/option[6]').click()
+elif priem_zv_c == '13:00:00':
+    browser.find_by_xpath('//*[@id="offer_experience_year_count"]/option[7]').click()
+elif priem_zv_c == '13:30:00':
+    browser.find_by_xpath('//*[@id="offer_experience_year_count"]/option[8]').click()
+elif priem_zv_c == '14:00:00':
+    browser.find_by_xpath('//*[@id="offer_experience_year_count"]/option[9]').click()
+elif priem_zv_c == '14:30:00':
+    browser.find_by_xpath('//*[@id="offer_experience_year_count"]/option[10]').click()
+elif priem_zv_c == '15:30:00':
+    browser.find_by_xpath('//*[@id="offer_experience_year_count"]/option[11]').click()
+elif priem_zv_c == '16:00:00':
+    browser.find_by_xpath('//*[@id="offer_experience_year_count"]/option[12]').click()
+elif priem_zv_c == '16:30:00':
+    browser.find_by_xpath('//*[@id="offer_experience_year_count"]/option[13]').click()
+elif priem_zv_c == '17:00:00':
+    browser.find_by_xpath('//*[@id="offer_experience_year_count"]/option[14]').click()
+elif priem_zv_c == '17:30:00':
+    browser.find_by_xpath('//*[@id="offer_experience_year_count"]/option[2]').click()
+elif priem_zv_c == '18:00:00':
+    browser.find_by_xpath('//*[@id="offer_experience_year_count"]/option[4]').click()
+elif priem_zv_c == '18:30:00':
+    browser.find_by_xpath('// *[ @ id = "offer_experience_year_count"] / option[5]').click()
+elif priem_zv_c == '19:00:00':
+    browser.find_by_xpath('//*[@id="offer_experience_year_count"]/option[6]').click()
+elif priem_zv_c == '19:30:00':
+    browser.find_by_xpath('//*[@id="offer_experience_year_count"]/option[7]').click()
+elif priem_zv_c == '20:00:00':
+    browser.find_by_xpath('//*[@id="offer_experience_year_count"]/option[8]').click()
+elif priem_zv_c == '20:30:00':
+    browser.find_by_xpath('//*[@id="offer_experience_year_count"]/option[9]').click()
+elif priem_zv_c == '21:00:00':
+    browser.find_by_xpath('//*[@id="offer_experience_year_count"]/option[10]').click()
+elif priem_zv_c == '21:30:00':
+    browser.find_by_xpath('//*[@id="offer_experience_year_count"]/option[11]').click()
+elif priem_zv_c == '22:00:00':
+    browser.find_by_xpath('//*[@id="offer_experience_year_count"]/option[12]').click()
+elif priem_zv_c == '22:30:00':
+    browser.find_by_xpath('//*[@id="offer_experience_year_count"]/option[13]').click()
+elif priem_zv_c == '23:00:00':
+    browser.find_by_xpath('//*[@id="offer_experience_year_count"]/option[14]').click()
+elif priem_zv_c == '23:30:00':
+    browser.find_by_xpath('//*[@id="offer_experience_year_count"]/option[2]').click()
 
-
-
+###############    дни для звонков    ###############
+browser.find_by_xpath('//*[@id="phoneContainerCallPeriod_"]').click()
 if opyt == 'Рабочие дни':
     browser.find_by_xpath('//*[@id="phoneContainerCallPeriod_"]/option[1]').click()
 elif opyt == 'Выходные дни':
@@ -191,4 +294,6 @@ elif opyt == 'Любой день':
 
 ###############    Phone    ###############
 
-# browser.quit()
+
+
+#browser.quit()
